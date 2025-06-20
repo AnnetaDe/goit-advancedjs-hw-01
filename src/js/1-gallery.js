603c9d5eaef7d650118d10e72c62f28d306029cc
AnnetaDe <anneta.liss@gmail.com>
2025-06-20 13:37:41 +0200

    5

---
+++ src/js/1-gallery.js
@@ -0,0 +1,78 @@
+export * from "./1-gallery.js";
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const images = [
+  {
+    preview:
+      "/img/gallery/1.png",
+    original:
+      "/img/gallery/1@2x.png",
+    description: "description 1"
+  },
+  {
+    preview:
+      "/img/gallery/1-1.png",
+    original:
+      "/img/gallery/1@2x-1.png",
+    description: "description 2"
+  },
+  {
+    preview:
+      "/img/gallery/2-1.png",
+    original:
+      "/img/gallery/2@2x-1.png",
+    description: "description 3"
+  },
+  {
+    preview:
+      "/img/gallery/2.png",
+    original:
+      "/img/gallery/2@2x.png",
+    description: "description 4"
+  },
+  {
+    preview:
+      "/img/gallery/3-1.png",
+    original:
+      "/img/gallery/3@2x-1.png",
+    description: "description 5"
+  },
+  {
+    preview:
+      "/img/gallery/3.png",
+    original:
+      "/img/gallery/3@2x.png",
+    description: "description 6"
+  },
+ 
+];
+
+const container =document.createElement("div")
+const gallery = document.querySelector(".gallery");
+if (!gallery) {
+  const galleryContainer = document.createElement("ul");
+  galleryContainer.classList.add("gallery");
+  document.body.appendChild(galleryContainer);
+}
+
+
+const createGalleryItem = images.map(item => {
+  return `<li class="gallery-item">
+    <a class="gallery-link" href="${item.original}">
+      <img class="gallery-image" src="${item.preview}" alt="${item.description}" />
+    </a>
+  </li>`;
+});
+
+gallery.insertAdjacentHTML("afterbegin", createGalleryItem.join(""));
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250
+});
+
+
---
+++ src/js/2-form.js
@@ -0,0 +1,51 @@
+export * from "./1-gallery.js";
+const formFeedback = document.querySelector(".feedback-form");
+const button = document.querySelector("button[type='submit']");
+button.className = "button";
+
+const emailInput = document.querySelector('input[type="email"]');
+emailInput.className = "input-email";
+
+const messageInput = document.querySelector("textarea[name='message']");
+messageInput.className = "input-message";
+
+const label = document.querySelector("label");
+label.className = "label";
+
+const user = {
+  email: "",
+  message: ""
+};
+
+const dataFromLocalStorage = localStorage.getItem("feedback-form-state");
+const parsedData = JSON.parse(dataFromLocalStorage);
+if (parsedData !== null) {
+  formFeedback.elements.email.value = parsedData.email;
+  formFeedback.elements.message.value = parsedData.message;
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (emailInput.value === "" || messageInput.value === "") {
+    alert("Please fill in all the fields!");
+    return;
+  }
+  console.log(localStorage.getItem("feedback-form-state"));
+  formFeedback.reset();
+  localStorage.removeItem("feedback-form-state");
+}
+
+function onFormInput(event) {
+  if (event.target === emailInput) {
+    user.email = emailInput.value.trim();
+    localStorage.setItem("feedback-form-state", JSON.stringify(user));
+  }
+  if (event.target === messageInput) {
+    user.message = messageInput.value.trim();
+    localStorage.setItem("feedback-form-state", JSON.stringify(user));
+  }
+}
+
+formFeedback.addEventListener("input", onFormInput);
+formFeedback.addEventListener("submit", onFormSubmit);
+
